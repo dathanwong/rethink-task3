9cf63745da0a0af952737bc2d44229b9ca731c30
dathanwong <dwong5@villanova.edu>
2020-09-21 19:30:36 -0700

    Got shortened url api working

--- server/controllers/url.controller.js
+++ server/controllers/url.controller.js
@@ -1,4 +1,5 @@
 const { Url } = require("../models/url.model");
+const domainName = "www.myshorturl.com";
 
 module.exports.index = (req, res)=>{
     res.json({
@@ -18,18 +19,17 @@ module.exports.findById =(req, res)=>{
         .catch(err => res.json(err));
 }
 
-module.exports.findByName = (req, res) =>{
-    Url.findOne({name: req.params.name})
-        .then(results =>{
-            res.json(results);
-        })
-        .catch(err => res.json(err));
-}
-
 module.exports.create = (req, res) =>{
     Url.create(req.body)
         .then(result =>{
-            res.json(result);
+            //Convert ID to hexadecimal string then update redirect in the db
+            let hex = Number(result._id).toString(16);
+            let redirect = `${domainName}/${hex}`;
+            Url.findByIdAndUpdate(result._id, {redirect: redirect})
+            .then(response =>{
+                res.json({redirect: redirect});
+            })
+            .catch(err => res.status(400).json(err));
         } )
         .catch(err => res.status(400).json(err));
 }
@@ -37,7 +37,6 @@ module.exports.create = (req, res) =>{
 module.exports.delete = (req, res) =>{
     Url.deleteOne({_id: req.params.id})
         .then(result =>{
-            initTrie();
             res.json(result);
         } )
         .catch(err => res.json(err));
@@ -46,27 +45,7 @@ module.exports.delete = (req, res) =>{
 module.exports.update = (req, res) =>{
     Url.updateOne({_id: req.params.id}, req.body, {new:true, runValidators: true})
         .then(result =>{
-            initTrie();
             res.json(result);
         })
         .catch(err => res.status(400).json(err));
-}
-
-module.exports.getSuggestions = (req, res) =>{
-    const results = ts.get(req.body.search);
-    res.json(results);
-}
-
-module.exports.getByPage = (req, res) =>{
-    Url.find().collation({locale:'en',strength: 2}).sort({name:1})
-    .then( (results) =>{ 
-        let resultsPerPage = 20;
-        let start = req.params.page*resultsPerPage;
-        let end = start + resultsPerPage;
-        let output = results.slice(start, end);
-        res.json(output);
-    })
-    .catch(err =>{
-        res.json(err);
-    });
 }
--- server/routes/url.routes.js
+++ server/routes/url.routes.js
@@ -2,14 +2,11 @@ const UrlController = require('../controllers/url.controller');
 const { Url } = require('../models/url.model');
 
 module.exports = function(app){
-    app.get('/api/url/page/:page', UrlController.getByPage);
-    app.get('/api/url/name/:name', UrlController.findByName);
     app.get('/api', UrlController.index);
     app.post('/api/url', UrlController.create);
     app.get('/api/url', UrlController.findAll);
     app.delete('/api/url/:id', UrlController.delete);
     app.put('/api/url/:id', UrlController.update);
     app.get('/api/url/:id', UrlController.findById);
-    app.post('/api/url/suggestions', UrlController.getSuggestions);
     
 }
